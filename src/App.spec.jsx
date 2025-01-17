e5866558e72bf6e2a5fd81dfb30460b4119580ce
Gabriel Fratelli <gabriel.fratelli99@gmail.com>
2025-01-17 16:51:10 -0300

    fix: trigger pipeline

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { App } from "./App";
 
 describe("Testing the Main Page <App />", () => {
   it("should display the text of the main page", () => {
